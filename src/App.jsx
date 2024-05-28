740f22eae4b0470d958542d6bc1b19c381623a09
dzackiero <dzakynashshar@gmail.com>
2024-05-29 00:35:32 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import mqtt from 'mqtt'
+
+function App() {
+  const [client, setClient] = useState(null)
+  const [isSubed, setIsSub] = useState(false)
+  const [payload, setPayload] = useState({})
+  const [connectStatus, setConnectStatus] = useState('Connect')
+
+  const mqttConnect = (host, mqttOption) => {
+    setConnectStatus('Connecting')
+    setClient(mqtt.connect(host, mqttOption))
+  }
+
+  useEffect(() => {
+    if (client) {
+      client.on('connect', () => {
+        setConnectStatus('Connected')
+        console.log('connection successful')
+      })
+
+      client.on('error', (err) => {
+        console.error('Connection error: ', err)
+        client.end()
+      })
+
+      client.on('reconnect', () => {
+        setConnectStatus('Reconnecting')
+      })
+
+      client.on('message', (topic, message) => {
+        const payload = { topic, message: message.toString() }
+        setPayload(payload)
+        console.log(`received message: ${message} from topic: ${topic}`)
+      })
+    }
+  }, [client])
+
+  const mqttDisconnect = () => {
+    if (client) {
+      try {
+        client.end(false, () => {
+          setConnectStatus('Connect')
+          console.log('disconnected successfully')
+        })
+      } catch (error) {
+        console.log('disconnect error:', error)
+      }
+    }
+  }
+
+  const mqttPublish = (context) => {
+    if (client) {
+      // topic, QoS & payload for publishing message
+      const { topic, qos, payload } = context
+      client.publish(topic, payload, { qos }, (error) => {
+        if (error) {
+          console.log('Publish error: ', error)
+        }
+      })
+    }
+  }
+
+  const mqttSub = (subscription) => {
+    if (client) {
+      // topic & QoS for MQTT subscribing
+      const { topic, qos } = subscription
+      // subscribe topic
+      // https://github.com/mqttjs/MQTT.js#mqttclientsubscribetopictopic-arraytopic-object-options-callback
+      client.subscribe(topic, { qos }, (error) => {
+        if (error) {
+          console.log('Subscribe to topics error', error)
+          return
+        }
+        console.log(`Subscribe to topics: ${topic}`)
+        setIsSub(true)
+      })
+    }
+  }
+
+  const mqttUnSub = (subscription) => {
+    if (client) {
+      const { topic, qos } = subscription
+      client.unsubscribe(topic, { qos }, (error) => {
+        if (error) {
+          console.log('Unsubscribe error', error)
+          return
+        }
+        console.log(`unsubscribed topic: ${topic}`)
+        setIsSub(false)
+      })
+    }
+  }
+
+  const handleConnect = () => {
+    const host = "broker.address.com";
+    const options = {
+      clientId: "emqx_react_" + Math.random().toString(16).substring(2, 8),
+      username: "react_username",
+      password: "react_password",
+    };
+    const topic = "topic/esp32";
+
+    mqttConnect(host, options);
+    mqttSub({topic: topic, qos: 0});
+  };
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleConnect}>
+          Connect!
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
